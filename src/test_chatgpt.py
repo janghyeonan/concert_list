4c95cac02c66a2abb24a0cad1189e6b7c2e75726
janghyeonan <ajh910@gmail.com>
2025-01-20 02:02:34 +0900

    add edit

--- src/test_chatgpt.py
+++ src/test_chatgpt.py
@@ -1,6 +1,7 @@
 from playwright.sync_api import sync_playwright
 import pandas as pd
 import time
+import os
 
 def test_chatgpt_homepage():
     with sync_playwright() as p:
@@ -9,18 +10,6 @@ def test_chatgpt_homepage():
         page.goto("https://www.naver.com")
         page.wait_for_load_state("load")
         time.sleep(10)
-
-        # 스크린샷 저장
-        page.screenshot(path="screenshot.png")
-
-        # 간단한 테스트: 제목 확인
-        assert "naver" in page.title(), "Page title does not contain 'ChatGPT'"
-        print("✅ naver 페이지 접속 완료")
-
-        assert 1 == 2, "테스트 실패"
-        assert 1 == 1, "테스트 성공"
-
-        browser.close()
 
         # result 리스트 생성 (20개의 데이터 추가)
         result = [
@@ -35,3 +24,20 @@ def test_chatgpt_homepage():
 
         # HTML 파일로 저장
         df.to_html("result22.html", index=False)
+
+        print("엑셀 파일 존재:", os.path.exists("result.xlsx"))
+        print("HTML 파일 존재:", os.path.exists("result22.html"))
+
+
+        # 스크린샷 저장
+        page.screenshot(path="screenshot.png")
+
+        # 간단한 테스트: 제목 확인
+        assert "naver" in page.title(), "Page title does not contain 'ChatGPT'"
+        print("✅ naver 페이지 접속 완료")
+
+        assert 1 == 2, "테스트 실패"
+        assert 1 == 1, "테스트 성공"
+
+        browser.close()
+
